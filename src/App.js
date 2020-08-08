8af933fe1ad41714dd6d1c387ca4511b55311266
avinashkranjan <ranjan.avinash@hotmail.com>
2020-08-08 07:50:24 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Let's Build a Covid-19 Informer</h1>
+    </div>
+  );
+}
+
+export default App;
