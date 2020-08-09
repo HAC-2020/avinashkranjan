f195aa7fc84e571340f110f4f90d81b83cd84cb0
avinashkranjan <ranjan.avinash@hotmail.com>
2020-08-09 10:32:38 +0530

    � Added New Features

---
+++ src/InfoBox.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
+
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        {/* title */}
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        {/* No. of cases */}
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
+        {/* Total */}
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
+
+export default InfoBox;
